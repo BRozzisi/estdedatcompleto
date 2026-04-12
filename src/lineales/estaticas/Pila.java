ab27e5a16bc77d1de42d0685699e8c01ee19d5f3
BRozzisi <brro895@gmail.com>
2026-04-12 15:14:34 -0300

    modified:   bin/lineales/estaticas/Pila.class modified:   src/lineales/estaticas/Pila.java

--- src/lineales/estaticas/Pila.java
+++ src/lineales/estaticas/Pila.java
@@ -1,81 +1,108 @@
 package lineales.estaticas;
+
+/**
+ * La clase Pila es una estructura de datos de tipo FILO
+ * Esta es una implementación estática de pila,
+ * 
+ * @author Bruno Rozzisi || FAI-5892
+ */
+
 public class Pila {
     private Object [] arreglo;
     private int tope;
     private final int TAMANIO = 20;
     
-    // Constructor vacío.
+    /**
+     * Constructor vacio.
+     */
     public Pila() {
         this.arreglo = new Object[TAMANIO]; 
         this.tope = -1;
     }
     
-    // Pone un objeto en el tope, le suma 1 al tope. Retorna true si la pila no estaba llena.
+    /**
+     * Si el arreglo tiene espacio, apila elemento en tope+1. Añade 1 a tope.
+     * @param elemento Elemento que queremos apilar.
+     * @return Devuelve true si la el arreglo tiene espacio para apilar. Si no
+     *         devuelve false.
+     */
     public boolean apilar(Object elemento) {
         boolean hayEspacio = true;
-        if (this.tope+1<this.TAMANIO) { // Añade el objeto solo si hay espacio en la pila.
+        if (this.tope+1<this.TAMANIO) {
             this.tope++;
             this.arreglo[tope] = elemento;
-        } else {                        // Devuelve false si está llena.
+        } else {
             hayEspacio = false;
         }
         return hayEspacio;
     }
 
-    // Pone nulo al objeto del tope, le resta 1 al tope. Devuelve true si la pila no estaba vacía
+    /**
+     * Si el arreglo tiene algún elemento, lo elimina de la pila y resta 1 a
+     * tope.
+     * @return Devuelve true si la pila tenía algun elemento.
+     */
     public boolean desapilar() {
-        if (!this.esVacia()) {        // Pone nulo solo si la pila no está vacía.
+        if (!this.esVacia()) {
             this.arreglo[tope] = null;
             this.tope--;
         }
         return !this.esVacia();
     }
     
-    // Devuelve el objeto que se encuentra en el tope.
+    /**
+     * 
+     * @return Devuelve el elemento en arreglo[tope], en caso de que la pila no
+     * tenga elementos, devuelve un objeto nulo.
+     */
     public Object obtenerTope() {
         Object objeto;
-        if (this.tope != -1){           // Revisa que la pila no esté vacía.
-            objeto = this.arreglo[tope];
+        if (this.tope != -1){
+            objeto = this.arreglo[this.tope];
         } else {
-            objeto = null;              // Si la pila está vacía devuelve nulo.
+            objeto = null;
         }
         return objeto;
     }
     
-    // Devuelve el valor del tope.
-    public int getTope() {
-        return this.tope;
-    }
-    
-    // Devuelve true si la pila no tiene ningún elemento.
+    /**
+     * @return Devuelve true si la pila no tiene ningún elemento.
+     */
     public boolean esVacia() {
         return this.tope == -1;
     }
     
-    // Resetea la pila a un estado vacío.
+    /**
+     * Vacía todos los espacios de la pila. Setea tope a -1.
+     */
     public void vaciar() {
-        if (!this.esVacia()) {               // Revisa que la pila no esté vacía.
+        if (!this.esVacia()) {
             int i;
-            for (i=0; i<this.TAMANIO; i++) { // Recorre toda la pila y le pone nulo a los valores.
+            for (i=0; i<this.TAMANIO; i++) {
                 this.arreglo[i] = null;
             }
             this.tope = -1;
         }
     }
     
-    // Devuelve un clon de la pila.
+    /**
+     * @return Devuelve una copia de la pila.
+     */
     @Override
     public Pila clone() {
         Pila pilaClon = new Pila();
-        if (!this.esVacia()) { // Copia el arreglo y el tope solo si la pila no está vacía.
+        if (!this.esVacia()) {
             pilaClon.arreglo = this.arreglo.clone();
             pilaClon.tope = this.tope;
         }
         return pilaClon;
     }
     
-    // Devuelve una cadena con todos los elementos actuales de la pila, desde el tope hasta el fondo.
-    // MÉTODO CON MOTIVOS DE DESARROLLO
+    /**
+     * @return Devuelve una cadena con todos los elementos de la pila. Si la
+     * pila está vacía devuelve "[]".
+     * @hidden
+     */
     @Override
     public String toString() {
         String cadena = "";
