57bec780fd61311de6765080c84af87660566199
BRozzisi <brro895@gmail.com>
2026-05-22 17:32:57 -0300

    modified:   bin/conjuntistas/ArbolBB.class modified:   src/conjuntistas/ArbolBB.java

--- src/conjuntistas/ArbolBB.java
+++ src/conjuntistas/ArbolBB.java
@@ -24,13 +24,15 @@ public class ArbolBB {
     private boolean insertarAux(NodoABB n, Comparable elemento) {
         boolean exito = true;
         if ((elemento.compareTo(n.getElem())) == 0) {
-            // Si el elemento es igual al del nodo actual, entonces retorna false ya que no puede haber 2 elementos
+            // Si el elemento es igual al del nodo actual, entonces retorna false ya que no
+            // puede haber 2 elementos
             // iguales en esta implementacion del ABB
             exito = false;
         } else if (elemento.compareTo(n.getElem()) < 0) {
             // Si el elemento es mas chico que el del nodo actual, pregunta:
             if (n.getIzquierdo() != null) {
-                // Si el nodo actual ya tiene HI entonces llama recursivamente con el para que lo inserte en su
+                // Si el nodo actual ya tiene HI entonces llama recursivamente con el para que
+                // lo inserte en su
                 // subarbol izquierdo.
                 exito = insertarAux(n.getIzquierdo(), elemento);
             } else {
@@ -40,7 +42,8 @@ public class ArbolBB {
         } else {
             // Si el elemento es mas grande que el del nodo actual, pregunta:
             if (n.getDerecho() != null) {
-                // Si el nodo actual tiene HD entonces llama recursivamente con el para que lo inserte en su subarbol
+                // Si el nodo actual tiene HD entonces llama recursivamente con el para que lo
+                // inserte en su subarbol
                 // derecho
                 exito = insertarAux(n.getDerecho(), elemento);
             } else {
@@ -59,7 +62,8 @@ public class ArbolBB {
         boolean esHijoIzq = false;
         int comparacion = 1;
         while ((n != null) && (comparacion != 0)) {
-            // Se baja por la rama correspondiente del arbol segun el valor de elemento hasta que se llegue al final o
+            // Se baja por la rama correspondiente del arbol segun el valor de elemento
+            // hasta que se llegue al final o
             // hasta que encontremos el elemento
             comparacion = elemento.compareTo(n.getElem());
             if (comparacion == 0) {
@@ -76,7 +80,8 @@ public class ArbolBB {
                 }
                 exito = true;
             } else if (comparacion < 0) {
-                // Si el elemento es menor al del nodo actual, entonces se baja por la rama izquierda
+                // Si el elemento es menor al del nodo actual, entonces se baja por la rama
+                // izquierda
                 padre = n;
                 n = n.getIzquierdo();
                 esHijoIzq = true;
@@ -93,11 +98,13 @@ public class ArbolBB {
 
     private void casoSinHijos(NodoABB padre, boolean esHijoIzq) {
         if (padre == null) {
-            // Si el nodo donde se encontro el elemento no tiene padre, significa que es la raiz, por lo tanto se pone
+            // Si el nodo donde se encontro el elemento no tiene padre, significa que es la
+            // raiz, por lo tanto se pone
             // nula y se vacia el arbol
             this.raiz = null;
         } else {
-            // Si el nodo donde se encontro el elemento si tiene padre, se fija si es el HI o HD de su padre y lo
+            // Si el nodo donde se encontro el elemento si tiene padre, se fija si es el HI
+            // o HD de su padre y lo
             // elimina
             if (esHijoIzq) {
                 padre.setIzquierdo(null);
@@ -111,7 +118,8 @@ public class ArbolBB {
         NodoABB padreCandidato = padre;
         NodoABB candidato = padre.getIzquierdo();
         if ((candidato.getDerecho()) != null) {
-            // Si el HI del nodo que se va a eliminar tiene HD, se recorre hasta que encontramos el mayor del subarbol
+            // Si el HI del nodo que se va a eliminar tiene HD, se recorre hasta que
+            // encontramos el mayor del subarbol
             // izquierdo del nodo a eliminar
             while (candidato.getDerecho() != null) {
                 padreCandidato = candidato;
@@ -120,8 +128,10 @@ public class ArbolBB {
             padre.setElem(candidato.getElem());
             padreCandidato.setDerecho(candidato.getIzquierdo());
         } else {
-            // Si el HI del nodo que se va a eliminar no tiene HD, entonces ese sera su reemplazante, se setean los
-            // parametros del padre (que en este caso es el nodo a eliminar) para que sea igual al nodo reemplazante
+            // Si el HI del nodo que se va a eliminar no tiene HD, entonces ese sera su
+            // reemplazante, se setean los
+            // parametros del padre (que en este caso es el nodo a eliminar) para que sea
+            // igual al nodo reemplazante
             padre.setElem(candidato.getElem());
             padre.setIzquierdo(candidato.getIzquierdo());
         }
@@ -131,16 +141,19 @@ public class ArbolBB {
         if (n.getIzquierdo() != null) {
             // Si el nodo a eliminar solo tiene HI pregunta:
             if (padre == null) {
-                // Si el padre es nulo, significa que el nodo a eliminar es la raiz, por lo que se le asigna a la raiz
+                // Si el padre es nulo, significa que el nodo a eliminar es la raiz, por lo que
+                // se le asigna a la raiz
                 // su propio HI.
                 this.raiz = this.raiz.getIzquierdo();
             } else {
                 // Si el padre no es nulo entonces pregunta:
                 if (esHijoIzq) {
-                    // Si es HI del padre entonces le asigna su subarbol izquierdo al izquierdo del padre
+                    // Si es HI del padre entonces le asigna su subarbol izquierdo al izquierdo del
+                    // padre
                     padre.setIzquierdo(n.getIzquierdo());
                 } else {
-                    // Si es HD del padre entonces le asigna su subarbol izquierdo al derecho del padre
+                    // Si es HD del padre entonces le asigna su subarbol izquierdo al derecho del
+                    // padre
                     padre.setDerecho(n.getIzquierdo());
                 }
             }
@@ -163,16 +176,20 @@ public class ArbolBB {
         int comparacion;
         NodoABB n = this.raiz;
         while ((n != null) && (!pertenece)) {
-            // Bajamos por la rama correspondiene hasta encontrar el elemento o hasta llegar al final
+            // Bajamos por la rama correspondiene hasta encontrar el elemento o hasta llegar
+            // al final
             comparacion = elemento.compareTo(n.getElem());
             if (comparacion < 0) {
-                // Si el elemento es mas chico que el del nodo actual entonces baja por el subarbol izquierdo
+                // Si el elemento es mas chico que el del nodo actual entonces baja por el
+                // subarbol izquierdo
                 n = n.getIzquierdo();
             } else if (comparacion > 0) {
-                // Si el elemento es mas grande que el del nodo actual entonces baja por el subarbol derecho
+                // Si el elemento es mas grande que el del nodo actual entonces baja por el
+                // subarbol derecho
                 n = n.getDerecho();
             } else {
-                // Si el elemento es igual al del nodo actual entonces se verifica que pertenece y retorna true.
+                // Si el elemento es igual al del nodo actual entonces se verifica que pertenece
+                // y retorna true.
                 pertenece = true;
             }
         }
@@ -191,7 +208,8 @@ public class ArbolBB {
 
     private void listarAux(NodoABB n, Lista l) {
         if (n != null) {
-            // Si el nodo actual no es nulo, le pide a su subarbol izquierdo que se liste, luego se lista a si mismo,
+            // Si el nodo actual no es nulo, le pide a su subarbol izquierdo que se liste,
+            // luego se lista a si mismo,
             // y luego le pide a su subarbol derecho que se liste
             listarAux(n.getIzquierdo(), l);
             l.insertar(n.getElem(), l.longitud() + 1);
@@ -209,19 +227,24 @@ public class ArbolBB {
         if (n != null) {
             // Si el nodo actual no es nulo pregunta:
             if (elemMin.compareTo(n.getElem()) <= 0) {
-                // Si el elemento minimo del rango es menor o igual al del nodo actual, entonces le pide a su subarbol
+                // Si el elemento minimo del rango es menor o igual al del nodo actual, entonces
+                // le pide a su subarbol
                 // izquierdo que se liste.
                 listarRangoAux(n.getIzquierdo(), l, elemMin, elemMax);
                 if (elemMax.compareTo(n.getElem()) >= 0) {
-                    // Si el elemento maximo del rango es mayor o igual al del nodo actual, se inserta a si mismo (ya
-                    // que por la condicion anterior se verifica que esta dentro del rango) y le pide a su subarbol
+                    // Si el elemento maximo del rango es mayor o igual al del nodo actual, se
+                    // inserta a si mismo (ya
+                    // que por la condicion anterior se verifica que esta dentro del rango) y le
+                    // pide a su subarbol
                     // derecho que se liste
                     l.insertar(n.getElem(), l.longitud() + 1);
                     listarRangoAux(n.getDerecho(), l, elemMin, elemMax);
                 }
             } else {
-                // Si el elemento minimo del rango es mayor al del nodo actual, entonces el actual es muy chico para
-                // entrar en el rango por lo que todo su subarbol izquierdo tambien lo sera. Unicamente pide a su
+                // Si el elemento minimo del rango es mayor al del nodo actual, entonces el
+                // actual es muy chico para
+                // entrar en el rango por lo que todo su subarbol izquierdo tambien lo sera.
+                // Unicamente pide a su
                 // subarbol derecho que se liste
                 listarRangoAux(n.getDerecho(), l, elemMin, elemMax);
             }
@@ -232,7 +255,8 @@ public class ArbolBB {
         Object rta = null;
         if (this.raiz != null) {
             NodoABB n = this.raiz;
-            // Si el arbol no esta vacio, baja por la rama extrema izquierda hasta llegar al final y retorna el elemento
+            // Si el arbol no esta vacio, baja por la rama extrema izquierda hasta llegar al
+            // final y retorna el elemento
             // del nodo hoja.
             while (n.getIzquierdo() != null) {
                 n = n.getIzquierdo();
@@ -246,7 +270,8 @@ public class ArbolBB {
         Object rta = null;
         if (this.raiz != null) {
             NodoABB n = this.raiz;
-            // Si el arbol no esta vacio, baja por la rama extrema derecha hasta llegar al final y retorna el elemento
+            // Si el arbol no esta vacio, baja por la rama extrema derecha hasta llegar al
+            // final y retorna el elemento
             // del nodo hoja.
             while (n.getDerecho() != null) {
                 n = n.getDerecho();
